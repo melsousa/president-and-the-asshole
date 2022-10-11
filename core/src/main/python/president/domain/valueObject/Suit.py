b59acf4ef803554b7d8effc7bda01451561050dc
Larabsg <laragomes2009@hotmail.com>
2022-10-11 12:38:10 -0300

    enum suit

---
+++ core/src/main/python/president/domain/valueObject/Suit.py
@@ -0,0 +1,12 @@
+import enum
+from ValueObject import ValueObject
+from enum import Enum
+
+
+class Suit(ValueObject, Enum):
+    
+    HEARTS = 1
+    DIAMONDS = 1
+    CLUBS = 1
+    SPADES = 1
+    
