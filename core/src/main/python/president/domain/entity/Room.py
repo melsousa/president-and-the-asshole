fc0cb546ce8c4c67991ce9f4e0277e82af28d53a
Melissa de Sousa <melissasousa6ms@gmail.com>
2022-12-01 11:09:05 -0300

    update room method choiceCard, dealCards

--- core/src/main/python/president/domain/entity/Room.py
+++ core/src/main/python/president/domain/entity/Room.py
@@ -39,33 +39,41 @@ class Room(Enum):
         self.cardsToChoice = []
         self.cardsToDeal = []
 
-        
         self.status = Status.WAITING
-    
+
     def dealCards(self):
-        pass
-        # qtyCardsOfRemove = (self.N_DECKS * 52) % self.players.__len__()
-        # card = 0
-        # for i in range(0, self.N_DECKS, 1):
-        #     for j in range(card:Deck.Deckof()._Card()):
-        #         if card.CardValue()
-    
-    def initializePlayers(self, players:Player[List]):
-        self.playersToChoice = [players.index(players.playerId(), 0, players.__len__())] # ?
+
+        qtyCardsOfRemove = (self.N_DECKS * 52) % self.players.__len__()
+        card = 0
         
+        for i in range(0, self.N_DECKS, 1):
+            for card in Deck.Deckof()._Card():
+                if card.CardValue().__eq__(CardValue.THREE) and qtyCardsOfRemove > 0:  # ?
+                    qtyCardsOfRemove -= 1
+        # ++
+        currentPlayer = 0
+        
+        for card in self.cardsToDeal:
+            pass
+            # self.players.get(currentPlayer).add
+
+    def initializePlayers(self, players: Player[List]):
+        self.playersToChoice = [players.index(
+            players.playerId(), 0, players.__len__())]  # ?
+
     def toSorting(self):
         if self.status != Status.WAITING:
-            print("Room is not waiting") # +
-        
+            print("Room is not waiting")  # +
+
         self.shuffleCardsToChoice()
         self.status = Status.IN_SORTING
-    
+
     def shuffleCardsToChoice(self):
         if self.players.__len__() < self.acessConfig.minplayers():
             raise RuntimeError("'max players' can't be less than four!")
-        
+
         # for(cardValue:CardValue.):
-           
+
     @staticmethod
     def ofRoom(owner: Player, acessConfig: AcessConfig):
         roomId = RoomId.ofRoomId()
@@ -75,14 +83,14 @@ class Room(Enum):
 
         return Room(roomId, owner, roomLink, acessConfig, players)
 
-    def choiceCard(self, player:Player):
+    def choiceCard(self, player: Player):
         if self.status != Status.IN_SORTING:
             print("Room is not in sorting")
-        
-        if (self.playerToChoice.__contains__(player.playerId())): # ++
-            print("Can't choice card for player "+ player.playerId().value)
-        
-        card = self.cardsToChoice.get(0) # está faltando código
+
+        if (self.playerToChoice.__contains__(player.playerId())):  # ++
+            print("Can't choice card for player " + player.playerId().value)
+
+        card = self.cardsToChoice.get(0)  # está faltando código
         player.choiceCard(card)
         self.cardsToChoice.remove(card)
         self.playersToChoice.remove(player.playerId())
@@ -91,12 +99,13 @@ class Room(Enum):
 
     def sortPlayers(self):
         if len(self.playersToChoice) == 0:
-            self.players.sort(Player) # faltando códogp
+            self.players.sort(Player)  # faltando códogp
             pass
 
     def toThrowing(self):
         if self.status != Status.IN_SORTING and self.status != Status.ROUND_FINISHED:
             pass
+
     def addPlayer(self, playerId: PlayerId):
         pass
         # if self.players.len() >= self.acessConfig.maxPlayers():
@@ -118,3 +127,7 @@ class Room(Enum):
 
     def players(self):
         return self.players
+
+    def cardsToDeal(self):
+        # return
+        pass
